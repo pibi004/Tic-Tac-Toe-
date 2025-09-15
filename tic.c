f13c0bfcfb4a84c092f62d0890ffb24fb1ae0cb4
Pibida <it25100990@my.sliit.lk>
2025-09-15 10:29:05 +0000

    tictac

--- tic.c
+++ tic.c
@@ -1,73 +1,80 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
 #include<time.h>
+#include<stdbool.h>
 
-//structure to hold the game information
-typedf struct{
-	char** board;           //2D array for the game board
-	int size;               //Board size
-	int current_player;     //current player( 0, 1, 2)
-	int number_of_players;  //number of players(2 or 3)
-	char symbols[3];        //player symbols( X, Y, Z)
-	int player_types[3];    //0 or 1
-	FILE* log_file;         //Pointer for logging file
-}
-GameState;
+#define Max_Players 3   //Maximum Players can play
+#define Max_Size 10    //Maximum board size
 
-//Function to initialize the game
-GameState* initialize_game(int size, int number_of_players){
-	GameState* game = malloc(sizeof(GameState));
-	if (!game){
-		printf("Memory allocation failes\n");
-		return NULL;
+//Function Prototype
+void displayWelcome();
+void initBoard(char board[Max_Size][Max_Size], int size);
+void printBoard(char board[Max_Size][Max_Size], int size);
+bool validMove(char board[Max_Size][Max_Size], int size , int row, int col);
+bool checkWinner(char board[Max_Size][Max_Size], int size, char mark);
+bool boardFilled(char board[Max_Size][Max_Size], int size);
+void humanTurn(char board[Max_Size][Max_Size], int size, char mark);
+void computerTurn(char board[Max_Size][Max_Size],int size, char mark, char opponent);
+int getPlayerType(char symbol);
+
+//Main Function
+int main(){
+	srand((unsigned int)time(NULL));  //Send the random number generator
+					  
+	int size, mode, totalPlayers;
+	char board[Max_Size][Max_Size];   //The game Board
+        char symbols[Max_Players] = {'X' , 'O' , 'Z'};
+	int playerType[Max_Players];	// 0 = human  1 = computer
+
+	displayWelcome();  //Show intro message
+			   
+	//Prompt user for board size and validate input
+	while (1) {
+		printf("Enter board size (3 - 10): ");
+		scanf("%d", &size );
+		if (size >= 3 && <= 10)break;
+		printf("Invalid Board Size. Please Choose betweeen 3 and 10\n");
 	}
 
-	//Allocate memory for the board
-	game->board = malloc(size * sizeof(char*));
-	if (!game->board){
-		printf("Memory allocation failed\n");
-		free(game); //if program cannot continue it cleans up the memory
-	        return NULL;
+	//User choose the game mode to play
+	printf("\nGame Modes:\n");
+	printf("1. Player vs Player\n");
+	printf("2. Player vs Computer\n");
+	printf("3. Multi Player( 3 Player )\n");
+
+	while (1) {
+		printf("Select game mode: ");
+		scanf("%d", &mode);
+		if (mode >= 1 && mode <= 3) break;
+		printf("Invalid option. Choose 1,2 or 3.\n");
 	}
 
-	for (int i = 0; i < size; i++){
-		game->board[i] = malloc(size * sizeof(char));
-		if(!game->board[i]){
-			printf("Memory allocation failed\n");
-			//Free previous allocated memory
-			for (int j = 0;j <i;j++){
-				free(game->board);
-			}
-			free(game->board);
-			free(game);
-			return NULL;
+	//Set number of players and assign player types
+	if ( mode == 1) {
+		totalPlayers = 2;
+		playerType[0] = 0; //player 1 is human
+		playerType[1] = 0; //player 2 is human
+	} else if ( mode == 2) {
+		totalPlayers = 2;
+		playerType[0] = 0;  //player is human
+		playerType[1] = 1;  //player is computer
+	} else {
+		totalPlayers = 3;
+		printf("\n Select player types: \n");
+		for (int i = 0; i <3; i++){
+			playerType[i] = getPlayerType(symbols[i]);
 		}
-
-		//Initialize board space with spaces
-		for(int j = 0; j < size ; j++){
-			game->board[i][j] = ' ';
-		}
 	}
 
-	game->size = size;
-	game->current_player = 0;
-	game->number_of_players = number_of_players;
+	initBoard(board, size); //setup empty board
+	int currentTurn = 0;	//Track current player's turn
+	bool gameOver = false;
 
-	//Give player symbols
-	game->symbols[0] = 'X';
-	game->symbols[1] = 'O';
-	game->symbols[2] = 'Z';
+	//Game Loop
+	while (!gameOver) {
+		printBoard(board, size);
+		printf("Player %d (%c)'s move\n", currentTurn + 1, symbols[currentTurn]);
+		
 
-	//Open logging files
-	game->log_file = fopen("game_log.txt", "w");
-	if(!game->log_file){
-		printf("Error\n");
-	}else {
-		fprintf(game->log_file, "Tic-Tac-Toe"\n);
-		fprintf(game->log_file, "Board Size: %d x %d\n", size, size);
-		fprintf(game->log_file, "Number Of Players: %d\n", number_of_players);
-	}
-	return game;
-}
-
+	        // Human or Computer takes a turn
+	        
